7f6bdde44b239ff03a980a8db5945afdaeb07775
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-10 01:24:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,11 @@
 require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db 
+<<<<<<< HEAD
   def initialize(db:, id:, name:, type:)
+=======
+  def initialize(db)
+>>>>>>> 83c55de0b709bf8323d43047acf525e0a079a81c
     @db = db
     self.id = id
     self.name = name 
@@ -11,6 +15,7 @@ class Pokemon
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) 
       VALUES (?, ?)", name, type) 
+<<<<<<< HEAD
   end 
   
   def self.find(id, db)
@@ -18,5 +23,7 @@ class Pokemon
     FROM pokemon
     WHERE id = ?", id).flatten
     Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db) 
+=======
+>>>>>>> 83c55de0b709bf8323d43047acf525e0a079a81c
   end 
 end
